ac101ad8dbc4d972713408faf4e8a68553c6c54b
Sengngy Kouch <sengngykouch@Sengngys-MacBook-Pro.local>
2020-11-19 14:01:00 -0500

    Add table and button to the Room component

--- ClientApp/src/app/room/room.component.ts
+++ ClientApp/src/app/room/room.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-room',
-  templateUrl: './room.component.html',
-  styleUrls: ['./room.component.css']
+    selector: 'app-room',
+    templateUrl: './room.component.html',
+    styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    roomList = [
+        {Id: 1, Building: 'House1', Room: 'Living Room'},
+        {Id: 2, Building: 'House1', Room: 'Kitchen'}
+    ];
+
+
+    /* ------------------
+        Modal Triggers
+    ---------------------*/
+    addRoomModal(): void {
+        // trigger the modal.
+        // reset all the field.
+    }
+
+    editRoomModal(): void {
+        // trigger the edit modal.
+        // get Room properties and fill in the space.
+    }
+
+    deleteRoomModal(): void {
+        // trigger the delete modal.
+    }
+
+    /* -----------------
+        Modal Buttons
+    --------------------*/
+    addRoom(): void {
+
+    }
+
+    updateRoom(): void {
+
+    }
+
+    deleteRoom(): void {
+        
+    }
+
 
 }
