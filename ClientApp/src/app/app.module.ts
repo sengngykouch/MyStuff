9cc00e8ecb0538dcd6bf98b839f48a82858e40df
Sengngy Kouch <kouchsengngy@gmail.com>
2020-11-19 22:47:25 -0500

    Added Material icons and buttons

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -16,6 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatTableModule } from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
@@ -41,7 +45,10 @@ import {MatSortModule} from '@angular/material/sort';
         ], { relativeLinkResolution: 'legacy' }),
         BrowserAnimationsModule,
         MatTableModule,
-        MatSortModule
+        MatSortModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule
     ],
     providers: [],
     bootstrap: [AppComponent]
