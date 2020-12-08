90735a0217c482c0ca091c12fb15d9596e6afc8f
sengngykouch <kouchsengngy@gmail.com>
2020-12-07 20:25:23 -0500

    Added filter to the material table.

--- TrackMyStuffApp/ClientApp/src/app/item/item.component.ts
+++ TrackMyStuffApp/ClientApp/src/app/item/item.component.ts
@@ -59,6 +59,15 @@ export class ItemComponent implements OnInit, AfterViewInit {
         this.dataSource.sort = this.sort;
     }
 
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
     getAllItems() {
         this.itemService.getItems().subscribe(items => {
             this.dataSource.data = items;
