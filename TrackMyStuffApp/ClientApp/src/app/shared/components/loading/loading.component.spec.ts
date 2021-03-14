d0fa229f0f110ef7efa19db97773c21ea13a5880
sengngykouch <kouchsengngy@gmail.com>
2021-03-14 19:36:29 -0400

    Added tests for Loading component.

--- TrackMyStuffApp/ClientApp/src/app/shared/components/loading/loading.component.spec.ts
+++ TrackMyStuffApp/ClientApp/src/app/shared/components/loading/loading.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
 
 import { LoadingComponent } from './loading.component';
 
-describe('LoadingComponent', () => {
+fdescribe('LoadingComponent', () => {
     let component: LoadingComponent;
     let fixture: ComponentFixture<LoadingComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LoadingComponent]
+            declarations: [LoadingComponent],
+            imports: [AngularMaterialModule]
         })
             .compileComponents();
     });
@@ -21,5 +24,23 @@ describe('LoadingComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show the mat Spinner when loading is true', () => {
+        component.isLoading = true;
+        fixture.detectChanges();
+
+        let matSpinner = fixture.debugElement.query(By.css('mat-spinner'));
+
+        expect(matSpinner).toBeTruthy();
+    });
+
+    it('should not show mat Spinner when loading is false', () => {
+        component.isLoading = false;
+        fixture.detectChanges();
+
+        let matSpinner = fixture.debugElement.query(By.css('mat-spinner'));
+
+        expect(matSpinner).toBeNull();
     });
 });
